6997fddcbc7dda0e90de38633fe4fa02eca09a45
ozy <ozyftw17@gmail.com>
2024-06-20 02:20:51 +0530

    ui fix lil bit

--- src/_Components/Layout.tsx
+++ src/_Components/Layout.tsx
@@ -4,13 +4,6 @@ import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren<{}>) => {
   return (
-    <main className="flex h-screen justify-center">
-      <div
-        className=" h-full w-full 
-        overflow-y-scroll  md:max-w-xl lg:max-w-xl"
-      >
-        {props.children}
-      </div>
-    </main>
+    <main className="flex h-screen  justify-center">{props.children}</main>
   );
 };
--- src/_Components/Post.tsx
+++ src/_Components/Post.tsx
@@ -5,25 +5,27 @@ const Post = async () => {
 
   return (
     <>
-      {[...posts].map((image) => (
-        <div className="mt-28 h-80 w-72 rounded-md border-2 border-white">
-          <div className=" flex flex-row rounded-md border-b-[1px] border-white py-2">
-            <Image src="/ninja.png" alt="" height={30} width={30}></Image>
-            <h1 className="text-lg font-normal text-white">user id</h1>
-          </div>
-          <div className="flex items-center justify-center">
-            <div className=" h-48 w-48" key={image.id}>
-              <Image
-                src={image.url || ""}
-                alt=""
-                width={480}
-                height={480}
-                style={{ objectFit: "contain" }}
-              />
+      <div className="mt-28">
+        {[...posts].map((image) => (
+          <div className="mt-12 h-80 w-72 rounded-md border-2 border-white">
+            <div className=" flex flex-row rounded-md border-b-[1px] border-white py-2">
+              <Image src="/ninja.png" alt="" height={30} width={30}></Image>
+              <h1 className="text-lg font-normal text-white">user id</h1>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className=" h-48 w-48" key={image.id}>
+                <Image
+                  src={image.url || ""}
+                  alt=""
+                  width={480}
+                  height={480}
+                  style={{ objectFit: "contain" }}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 };
