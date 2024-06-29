f7254ddf8336912046c7b0e4ba2d461ca0aee20d
ozy <ozyftw17@gmail.com>
2024-06-30 03:22:45 +0530

    nav button fixed

--- src/_Components/NavBar.tsx
+++ src/_Components/NavBar.tsx
@@ -9,6 +9,7 @@ import { SimpleUploadButton } from "../app/posts/simple-upload-button";
 import { SquarePlus } from "lucide-react";
 import { DialogCloseButton } from "../app/posts/CaptionModal";
 import { ModeToggle } from "./darkModeToggle";
+import { Button } from "~/components/ui/button";
 import { NewPostDialog } from "~/app/posts/NewPostDialog";
 type Props = {
   className?: string;
@@ -41,14 +42,18 @@ const TopNav = ({ className }: Props) => {
         ) : (
           <div className="max-w-18">
             <SignInButton>
-              <aside className="flex items-center gap-4">
+              {/* <aside className="flex items-center gap-4">
                 <div className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                   <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                     {"Sign In"}
                   </span>
                 </div>
-              </aside>
+              </aside> */}
+              <Button className="text-white" variant="outline">
+                {" "}
+                {"Sign In"}
+              </Button>
             </SignInButton>
           </div>
         )}
